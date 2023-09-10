397dd39d9611263205513fc2ddba68dfa7291c03
HarmanSandhu179 <harmansandhu179@outlook.com>
2023-09-09 23:40:30 -0700

    Add JS and a button that scrolls to the top

---
+++ script.js
@@ -0,0 +1,16 @@
+const scrollUpButton = document.getElementById("scrollUpBtn");
+
+// Show the button when the user scrolls down 200 pixels
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        scrollUpButton.style.display = "block";
+    } else {
+        scrollUpButton.style.display = "none";
+    }
+};
+
+// Scroll to the top when the button is clicked
+scrollUpButton.addEventListener("click", function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
