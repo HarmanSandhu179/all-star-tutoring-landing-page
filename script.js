95c414e1c9fb781fa37bc1330654f33e30546878
HarmanSandhu179 <harmansandhu179@outlook.com>
2024-01-07 14:13:35 -0800

    added text

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ const scrollUpButton = document.getElementById("scrollUpBtn");
 
 // Show the button when the user scrolls down 200 pixels
 window.onscroll = function () {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        scrollUpButton.style.display = "block";
-    } else {
-        scrollUpButton.style.display = "none";
-    }
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    scrollUpButton.style.display = "block";
+  } else {
+    scrollUpButton.style.display = "none";
+  }
 };
 
 // Scroll to the top when the button is clicked
 scrollUpButton.addEventListener("click", function () {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
 });
